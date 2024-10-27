47795b74e39a920b37539ed067ade74ef9a0d43e
Bayan Elian Alreqeb <104948334+se-bayan-elian@users.noreply.github.com>
2024-10-27 18:51:53 +0200

    Update page.tsx

--- app/payment/callback/page.tsx
+++ app/payment/callback/page.tsx
@@ -77,9 +77,31 @@ const PaymentCallback: React.FC = () => {
 // Wrap the component with Suspense
 const PaymentCallbackWrapper = () => {
   return (
-    <Suspense fallback={<div>Loading payment details...</div>}>
+    <Suspense fallback={<></><div className="loader"></div>
+       <style jsx>{`
+        .loader {
+          border: 8px solid #f3f3f3;
+          border-top: 8px solid #564495;
+          border-radius: 50%;
+          width: 60px;
+          height: 60px;
+          animation: spin 1s linear infinite;
+        }
+
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+       </>}>
       <PaymentCallback />
     </Suspense>
+
+
   );
 };
 
