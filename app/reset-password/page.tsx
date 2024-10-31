0ea00664a5246a7435284bae00e4f335f254a819
joe joe <youssefsherif469@gmail.com>
2024-10-31 11:32:24 +0300

    removed export beside wrap function

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -8,7 +8,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
-export function WrapResetPassword() {
+ function WrapResetPassword() {
   const [newPassword, setNewPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [showPassword, setShowPassword] = useState(false)
